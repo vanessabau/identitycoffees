dbf5f11620a855ecb162ad0eef77fad61dbab8df
Vanessa Bautista <vbautista916@icloud.com>
2020-10-10 08:08:42 -0700

    setting up server

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+// Setting up port and requiring models for syncing
+const PORT = process.env.PORT || 3000;
+// const db = require("./models");
+
+// Creating express app and configuring middleware needed for authentication
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+//Routes
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+
+app.listen(3000, function () {
+  console.log("App listening on port " + PORT);
+});
